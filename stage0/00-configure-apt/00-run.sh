96d492fe51f1209747ccac444592fb437d82f8a3
Josef Matondang <josstemat@gmail.com>
2020-02-26 15:28:22 +0700

    Add debian.public.key

--- stage0/00-configure-apt/00-run.sh
+++ stage0/00-configure-apt/00-run.sh
@@ -10,6 +10,7 @@ else
 	rm -f "${ROOTFS_DIR}/etc/apt/apt.conf.d/51cache"
 fi
 
+on_chroot apt-key add - < files/debian.public.key
 on_chroot apt-key add - < files/raspberrypi.gpg.key
 on_chroot << EOF
 apt-get update
